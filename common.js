4d1c9f853b667c2b24c92e19e162281b99df6f22
Kehan <kehanhetti@gmail.com>
2020-10-25 14:41:09 -0700

    AnimeBrowser

---
+++ common.js
@@ -0,0 +1,9 @@
+document.body.innerHTML = `<a href="Web App Project.html" class="title">
+      <h1 class="center">AnimeFinder</h1>
+    </a>
+    <div class="topnav">
+      <a href="Web App Project.html">Home</a>
+      <a href="About.html">About</a>
+      <a href="Streaming services.html">Streaming Services</a>
+      <a href="AnimeBrowser.html">Latest Released</a>
+    </div>`+ document.body.innerHTML
---
+++ data.js
@@ -0,0 +1,79 @@
+var shows = [
+ 
+    {
+      name: "Tokyo Ghoul",
+      description:
+        "Tokyo Ghoul is set in an alternate reality where ghouls, creatures that look like normal people but can only survive by eating human flesh, live amongst the human population in secrecy, hiding their true nature in order to evade pursuit from the authorities.",
+      image: "TG.jpg",
+      latestEpisodeDate: "December 25 2018",
+      genre: "Horror",
+    },
+    {
+      name: "The Promised Neverland",
+      description:
+        "The Promised Neverland follows three protagonists, Emma, Norman, and Ray, along with a cast of orphans who live together. Their lives have been full of joy, that is until the protagonists learn the orphanage is really a farm where children are raised and given to demons to be eaten.",
+      image: "TPN.jpg",
+      latestEpisodeDate: "March 29 2019",
+      genre: "Horror",
+    },
+    {
+      name: " Death Note",
+      description:
+        "Death Note (デスノート, Desu Nōto) is a manga series about a high school student who discovers a supernatural notebook that allows him to kill anyone by writing the victim's name while picturing their face.",
+      image: "DN.jpg",
+      latestEpisodeDate: "June 26 2007",
+      genre: "Horror",
+    },
+  
+    {
+      name: "My Hero Academia",
+      description:
+        "The story follows Izuku Midoriya, a boy born without superpowers (called Quirks) in a world where they have become commonplace, but who still dreams of becoming a hero himself.",
+      image: "MHA.jpg",
+      latestEpisodeDate: "April 4 2020",
+      genre: "Action",
+    },
+    {
+      name: "One punch man",
+      description:
+        "One-Punch Man is a Japanese superhero franchise created by the artist ONE. It tells the story of Saitama, a superhero who can defeat any opponent with a single punch but seeks to find a worthy foe after growing bored by a lack of challenge due to his overwhelming strength.",
+      image: "OPM.jpg",
+      latestEpisodeDate: "December 21 2015",
+      genre: "Action",
+    },
+    {
+      name: "Naruto",
+      description:
+        "Naruto (Japanese: ナルト) is a Japanese manga series written and illustrated by Masashi Kishimoto. It tells the story of Naruto Uzumaki, a young ninja who seeks recognition from his peers and dreams of becoming the Hokage, the leader of his village.",
+      image: "NS.png",
+      latestEpisodeDate: "March 23 2017",
+      genre: "Action",
+    },
+  
+  
+    {
+      name: "Haikyuu",
+      description:
+        "Determined to be like the volleyball championship's star player Shouyou, a short boy nicknamed the small giant, joins his school's volleyball club. Hinata Shouyou, a short middle school student, gained a sudden love of volleyball after watching a national championship match on TV.",
+      image: "H.jpg",
+      latestEpisodeDate: "October 16 2020",
+      genre: "Sports",
+    },
+    {
+      name: "Kuroko no basket",
+      description:
+        "A legendary 6th player. An up-and-coming power player, Taiga Kagami, is just back from America. When he comes to Seirin High School, he meets the super-ordinary boy, Tetsuya Kuroko. ... Kuroko makes a pact with Kagami to defeat the other members of the Miracle Generation, who have all played basketball at other schools.",
+      image: "KNB.jpg",
+      latestEpisodeDate: "September 22 2012",
+      genre: "Sports",
+    },
+    {
+      name: "Free Eternal Summer",
+      description:
+        "Eternal Summer- is set one year after the first season of Free!. The events revolve around Haruka Nanase, Makoto Tachibana, Rin Matsuoka, and Rin's childhood friend, Sosuke Yamazaki, in their final year of high school. The boys struggle with their future dreams in preparation for graduation.",
+      image: "FES.jpg",
+      latestEpisodeDate: "September 24 2014",
+      genre: "Sports",
+    },
+ 
+];
